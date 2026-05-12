ece0e2f6656c66f6481b011011b0948e100e4096
Konstantinv <yetifor@mail.ru>
2026-05-12 18:42:55 +0300

    add alerts test

---
+++ tests/alert_test.py
@@ -0,0 +1,43 @@
+from pages.js_alerts_page import JsAlertsPage, AlertsEnums
+from ui.page_actions import PageActions
+from enum import StrEnum
+from playwright.sync_api import Page
+from faker import Faker
+import logging
+from utils.logger import LOGGER_NAME
+
+fake = Faker()
+logger = logging.getLogger(LOGGER_NAME)
+
+
+class TestsJsAlerts:
+
+    def test_alerts(self, page):
+        page_action = PageActions(page)
+        alerts_page = JsAlertsPage(page)
+        logger.info(f'Переход по ссылке {AlertsEnums.URL}')
+        page.goto(AlertsEnums.URL)
+        logger.info('Клик по кнопке \'Click for JS Alert\'')
+        message = page_action.run_and_accept_alert(alerts_page.click_js_alert_button)
+        assert message == AlertsEnums.JS_ALERT, \
+            f'Ожидался текст:{AlertsEnums.JS_ALERT}, получен: {message}'
+        result = alerts_page.get_result_text()
+        assert result == AlertsEnums.JS_ALERT_RESULT, \
+            f'Ожидался резултат: {AlertsEnums.JS_ALERT_RESULT}, получен: {result}'
+        logger.info('Клик по кнопке \'Click for JS Confirm\'')
+        message = page_action.run_and_accept_alert(alerts_page.click_js_confirm_button)
+        assert message == AlertsEnums.JS_CONFIRM, \
+            f'Ожидался текст: {AlertsEnums.JS_CONFIRM}, получен: {message}'
+        result = alerts_page.get_result_text()
+        assert result == AlertsEnums.JS_CONFIRM_RESULT, \
+            f'Ожидался Результат: {AlertsEnums.JS_CONFIRM_RESULT}, получен: {result}'
+        random_word = fake.word()
+        logger.info(f'Сгенерировано слово:{random_word}')
+        logger.info('Клик по кнопке \'Click for JS Prompt\'')
+        message = page_action.run_and_accept_prompt(alerts_page.click_js_prompt_button, prompt_text=random_word)
+        assert message == AlertsEnums.JS_PROMPT,\
+            f'Ожидался текст: {AlertsEnums.JS_PROMPT}, получен:  {message}'
+        result = alerts_page.get_result_text()
+        assert result == AlertsEnums.JS_PROMPT_RESULT + random_word,\
+            f'Ожидался результат: {AlertsEnums.JS_PROMPT_RESULT + random_word}, получен: {result}'
+
--- tests/basic_aithorication_test.py
+++ tests/basic_aithorication_test.py
@@ -1,6 +1,6 @@
 from pages.basic_authorization_page import BasicAuthorizationPage
 from utils.url_utils import embed_credentials_in_url
-from utils.enums_data import DataEnum
+from utils.enums_data import BasicAuthorizationEnums
 from playwright.sync_api import Page
 
 
@@ -8,14 +8,14 @@ class TestBasicAuthorization:
 
     def test_basic_authorization(self,page):
         test_zone = embed_credentials_in_url(
-            DataEnum.TEST_BY_BASIC_AUTH_URL,
-            DataEnum.TEST_BY_BASIC_AUTH_USERNAME,
-            DataEnum.TEST_BY_BASIC_AUTH_PASSWORD
+            BasicAuthorizationEnums.TEST_BY_BASIC_AUTH_URL,
+            BasicAuthorizationEnums.TEST_BY_BASIC_AUTH_USERNAME,
+            BasicAuthorizationEnums.TEST_BY_BASIC_AUTH_PASSWORD
             )
         page.goto(test_zone)
         authorization_page = BasicAuthorizationPage(page)
         result = authorization_page.get_expected_message()
-        assert DataEnum.TEST_BY_BASIC_AUTH_EXPECTATIONS in result,(
+        assert BasicAuthorizationEnums.TEST_BY_BASIC_AUTH_EXPECTATIONS in result,(
             f'Test failed\n'
-            f'Expected text {DataEnum.TEST_BY_BASIC_AUTH_EXPECTATIONS}\n'
+            f'Expected text {BasicAuthorizationEnums.TEST_BY_BASIC_AUTH_EXPECTATIONS}\n'
             f'Actual text {result}')
